0178dce95e603d3f0a9b7a672eed5441c8d46f75
GGreen <gerrit.green@ds.mpg.de>
2018-06-11 15:47:45 +0200

    Modified last print

--- hello_world_git.py
+++ hello_world_git.py
@@ -2,4 +2,4 @@ if __name__== "__main__" :
     print("Hello, world!")
     print("Starting to understand git. This is my second commit!")
     print("Hello, world, again!")
-    print("I think I already understood the basic concepct of add and commit.")
+    print("I think I already understood the basic concepct of add and commit. Really?")
