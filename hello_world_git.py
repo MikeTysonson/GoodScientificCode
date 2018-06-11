5d70bd67e3ef8ae2d887fcbd60317d9afdcf907c
GGreen <gerrit.green@ds.mpg.de>
2018-06-11 17:01:34 +0200

    ignore

--- hello_world_git.py
+++ hello_world_git.py
@@ -1,12 +1,12 @@
 import monkey
-def print_monkeys(a):
-    s=t=u='\n'
-    i=0
-    for i in range(len(a)):
-        s += (("_,o=+#-*|_)(x@(>'^)\\&_,o=+#**|/)(x_(X*^||&_,o=+#~*|7)"+ "(x__<`^(/&")[a[i]::21]+ "*-" * (a[i] == 6)).center(14)
-        t += "    (o o)     "
-        u += "ooO--(_)--Ooo-"
-    print(s+t+u[-1])
+# def print_monkeys(a):
+#     s=t=u='\n'
+#     i=0
+#     for i in range(len(a)):
+#         s += (("_,o=+#-*|_)(x@(>'^)\\&_,o=+#**|/)(x_(X*^||&_,o=+#~*|7)"+ "(x__<`^(/&")[a[i]::21]+ "*-" * (a[i] == 6)).center(14)
+#         t += "    (o o)     "
+#         u += "ooO--(_)--Ooo-"
+#     print(s+t+u[-1])
 
 if __name__== "__main__" :
     print("Hello, world!")
@@ -14,4 +14,4 @@ if __name__== "__main__" :
     print("Hello, world, again!")
     print("I think I already understood the basic concepct of add and commit. Really?")
 
-    print_monkeys([0,1,2,3,4,5])
+    monkey.print_monkeys([0,1,2,3,4,5])
