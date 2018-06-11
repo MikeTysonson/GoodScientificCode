0d8d9bd6f8829e4167103c4f7b1137c164a6327b
GGreen <gerrit.green@ds.mpg.de>
2018-06-11 16:47:10 +0200

    added a method to print a monkey

--- hello_world_git.py
+++ hello_world_git.py
@@ -1,5 +1,17 @@
+import monkey
+def print_monkeys(a):
+    s=t=u='\n'
+    i=0
+    for i in range(len(a)):
+        s += (("_,o=+#-*|_)(x@(>'^)\\&_,o=+#**|/)(x_(X*^||&_,o=+#~*|7)"+ "(x__<`^(/&")[a[i]::21]+ "*-" * (a[i] == 6)).center(14)
+        t += "    (o o)     "
+        u += "ooO--(_)--Ooo-"
+    print(s+t+u[-1])
+
 if __name__== "__main__" :
     print("Hello, world!")
     print("Starting to understand git. This is my second commit!")
     print("Hello, world, again!")
     print("I think I already understood the basic concepct of add and commit. Really?")
+
+    print_monkeys([0,1,2,3,4,5])
