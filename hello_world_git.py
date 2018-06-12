14baaca450648fa08c2597b19100ff8d55daa063
GGreen <gerrit.green@ds.mpg.de>
2018-06-12 11:14:21 +0200

    print rabbits

--- hello_world_git.py
+++ hello_world_git.py
@@ -8,12 +8,23 @@ def print_palmtress(num):
         sys.stdout.write('  | ')
     sys.stdout.write('\n\n')
 
+def print_rabbit(num):
+    for i in range(num):
+        sys.stdout.write(' \\/ ')
+    sys.stdout.write('\n')
+    for i in range(num):
+        sys.stdout.write(' oo ')
+    sys.stdout.write('\n')
+    for i in range(num):
+        sys.stdout.write(' ** ')
+    sys.stdout.write('\n')
 
 if __name__== "__main__" :
-    print("Hello, world!")
+    print("Bye, monkeys")
     print("Starting to understand git. This is my second commit!")
     print("Hello, world, again!")
     print("I think I already understood the basic concepct of add and commit. Really?")
 
     monkey.print_monkeys([0,1,2,3,4,5])
     print_palmtress(10)
+    print_rabbit(10)
