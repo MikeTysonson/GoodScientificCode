a7c38022cc3d65b96ac107184b836bc64663d0d0
GGreen <gerrit.green@ds.mpg.de>
2018-06-12 10:44:25 +0200

    delete some comments

--- hello_world_git.py
+++ hello_world_git.py
@@ -1,12 +1,4 @@
 import monkey
-# def print_monkeys(a):
-#     s=t=u='\n'
-#     i=0
-#     for i in range(len(a)):
-#         s += (("_,o=+#-*|_)(x@(>'^)\\&_,o=+#**|/)(x_(X*^||&_,o=+#~*|7)"+ "(x__<`^(/&")[a[i]::21]+ "*-" * (a[i] == 6)).center(14)
-#         t += "    (o o)     "
-#         u += "ooO--(_)--Ooo-"
-#     print(s+t+u[-1])
 
 if __name__== "__main__" :
     print("Hello, world!")
